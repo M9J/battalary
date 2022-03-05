5613955221d31398643c473b75b51fde55f7c669
George M. Jay <mnsh.exe@gmail.com>
2022-03-05 21:55:56 +0530

    fixes

--- app.js
+++ app.js
@@ -2,70 +2,113 @@ console.log('Battalary version 3');
 console.log('Last build: 2022-03-05T15:17:22.049Z');
 
 let isMonitoring = false;
+let batteryRef = null;
+let notificationAudio = null;
+const notificationAudioUrl = 'audio/oy-oy.mp3';
+const btnStart = document.getElementById('start-button');
+const btnStop = document.getElementById('stop-button');
+const lblBatteryIsCharging = document.getElementById('battery-is-charging');
+const lblBatteryLevel = document.getElementById('battery-level');
+const lblBatteryChargingTime = document.getElementById('battery-charging-time');
+const lblBatteryDischargingTime = document.getElementById(
+  'battery-discharging-time'
+);
 
 function startMonitoring() {
-  if (isMonitoring) return false;
-  navigator.getBattery().then((battery) => {
-    updateAllBatteryInfo();
-    function updateAllBatteryInfo() {
-      updateChargeInfo();
-      updateLevelInfo();
-      updateChargingInfo();
-      updateDischargingInfo();
-    }
-
-    battery.addEventListener('chargingchange', () => {
-      updateChargeInfo();
+  if (!batteryRef) {
+    navigator.getBattery().then((battery) => {
+      batteryRef = battery;
+      updateAllBatteryInfo();
+      batteryRef.addEventListener('chargingchange', chargingchange);
+      batteryRef.addEventListener('levelchange', levelchange);
+      batteryRef.addEventListener('chargingtimechange', chargingtimechange);
+      batteryRef.addEventListener(
+        'dischargingtimechange',
+        dischargingtimechange
+      );
+      isMonitoring = true;
+      btnStart.classList.add('hidden');
+      btnStop.classList.remove('hidden');
     });
+  }
+}
 
-    function updateChargeInfo() {
-      document.getElementById('battery-is-charging').innerHTML =
-        battery.charging ? 'Yes' : 'No';
-    }
+function stopMonitoring() {
+  if (batteryRef) {
+    pauseAudio();
+    batteryRef.removeEventListener('chargingchange', chargingchange);
+    batteryRef.removeEventListener('levelchange', levelchange);
+    batteryRef.removeEventListener('chargingtimechange', chargingtimechange);
+    batteryRef.removeEventListener(
+      'dischargingtimechange',
+      dischargingtimechange
+    );
+    batteryRef = null;
+    lblBatteryIsCharging.innerHTML = '-';
+    lblBatteryLevel.innerHTML = '-';
+    lblBatteryChargingTime.innerHTML = '-';
+    lblBatteryDischargingTime.innerHTML = '-';
+    isMonitoring = false;
+    btnStop.classList.add('hidden');
+    btnStart.classList.remove('hidden');
+  }
+}
 
-    battery.addEventListener('levelchange', () => {
-      updateLevelInfo();
-      playAudio();
-    });
+function updateAllBatteryInfo() {
+  chargingchange();
+  levelchange();
+  chargingtimechange();
+  dischargingtimechange();
+}
 
-    function updateLevelInfo() {
-      document.getElementById('battery-level').innerHTML =
-        battery.level * 100 + '%';
-      const notifTitle = 'Battalary';
-      const notifBody = `Level: ${battery.level * 100}%`;
-      const notifImg = `icons/icon-64.png`;
-      const options = {
-        body: notifBody,
-        icon: notifImg,
-      };
-      new Notification(notifTitle, options);
-    }
+function chargingchange() {
+  lblBatteryIsCharging.innerHTML = batteryRef.charging ? 'Yes' : 'No';
+}
 
-    battery.addEventListener('chargingtimechange', () => {
-      updateChargingInfo();
-    });
+function levelchange() {
+  lblBatteryLevel.innerHTML = (batteryRef.level * 100).toFixed() + '%';
+  pushNotification();
+  const tmr1 = setTimeout(() => {
+    clearTimeout(tmr1);
+    playAudio();
+  }, 1000);
+}
 
-    function updateChargingInfo() {
-      document.getElementById('battery-charging-time').innerHTML =
-        battery.chargingTime + ' seconds';
-    }
+function chargingtimechange() {
+  lblBatteryChargingTime.innerHTML = batteryRef.chargingTime + ' seconds';
+}
 
-    battery.addEventListener('dischargingtimechange', () => {
-      updateDischargingInfo();
-    });
+function dischargingtimechange() {
+  lblBatteryDischargingTime.innerHTML = batteryRef.dischargingTime + ' seconds';
+}
 
-    function updateDischargingInfo() {
-      document.getElementById('battery-discharging-time').innerHTML =
-        battery.dischargingTime + ' seconds';
-    }
-  });
+function pushNotification() {
+  const notifTitle = 'Battalary';
+  const notifBody = `Level: ${(batteryRef.level * 100).toFixed()}%`;
+  const notifImg = `icons/icon-64.png`;
+  const options = {
+    body: notifBody,
+    icon: notifImg,
+  };
+  new Notification(notifTitle, options);
 }
 
 function playAudio() {
-  new Audio('audio/oy-oy.mp3').play();
+  if (!notificationAudio) {
+    notificationAudio = new Audio(notificationAudioUrl);
+    notificationAudio.play();
+  }
+}
+
+function pauseAudio() {
+  if (notificationAudio) {
+    notificationAudio.pause();
+    notificationAudio = null;
+  }
 }
 
 Notification.requestPermission().then((result) => {
   if (result === 'granted') {
+    console.log('Notification permission granted');
   }
 });
